f792af2a0cdaa746f6e420f97a97c9bfd0f33875
Vu Viet Hung <vuviethung3498@gmail.com>
2022-10-05 20:22:39 +0700

    done header

--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -1,15 +1,22 @@
 import styles from '@/components/Layout/DefaultLayout/Header/style.module.css';
 import { Link } from 'react-router-dom';
 import logo from '@/upload/img/logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 const Header = () => {
   return (
     <>
       <header className={styles.header}>
         <div className="container">
           <nav className={styles.headerNav}>
-            <Link to={'/'} className={styles.logoLink}>
+            <a
+              href={'https://template-bat-dong-san.mysapo.net/'}
+              target="_blank"
+              rel="noreferrer"
+              className={styles.logoLink}
+            >
               <img src={logo} className="header-logo" alt={logo} />
-            </Link>
+            </a>
             <ul className={styles.listNav}>
               <li className={styles.navItem}>
                 <Link to={'/'} className={styles.navLink}>
@@ -21,10 +28,50 @@ const Header = () => {
                   Giới thiệu
                 </Link>
               </li>
-              <li className={styles.navItem}>
+              <li className={styles.navItem + ' ' + styles.hasSub}>
                 <Link to={'/'} className={styles.navLink}>
                   Sản phẩm
+                  <FontAwesomeIcon icon={faChevronDown} style={{ marginLeft: '10px' }} />
                 </Link>
+                <div className={styles.hasPerspative + ' ' + styles.subMenu}>
+                  <ul className={styles.subMenu}>
+                    <li>
+                      <Link to={'/'} className={styles.subNavLink}>
+                        Căn hộ chung cư
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to={'/'} className={styles.subNavLink}>
+                        Nhà riêng
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to={'/'} className={styles.subNavLink}>
+                        Biệt thự liền kề
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to={'/'} className={styles.subNavLink}>
+                        Nhà mặt phố
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to={'/'} className={styles.subNavLink}>
+                        Đất nền dự án
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to={'/'} className={styles.subNavLink}>
+                        Trang trại, khu nghỉ dưỡng
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to={'/'} className={styles.subNavLink}>
+                        Đất nền, kho, nhà xưởng
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
               </li>
               <li className={styles.navItem}>
                 <Link to={'/'} className={styles.navLink}>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,13 @@
 import Header from '@/components/Layout/DefaultLayout/Header';
-
+import slider from '@/upload/img/slider_1.jpg';
+import style from './style.module.css';
 const Home = () => {
   return (
     <>
       <Header></Header>
+      <div className={style.sliderTop}>
+        <img src={slider} alt="" />
+      </div>
     </>
   );
 };
